f9458416d44669eac1a4ddcd81381f4e782fca58
HollaFoil <nedas60@gmail.com>
2026-09-01 17:08:10 +0200

    Replace starship with oh-my-posh; remove starship config

--- files/.config/hypr/hyprland.lua
+++ files/.config/hypr/hyprland.lua
@@ -72,6 +72,7 @@ local menu = "fuzzel"
 -- end)
 
 hl.on("hyprland.start", function()
+    hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP && systemctl --user restart hyprpolkitagent")
     hl.exec_cmd("swaync")
     hl.exec_cmd("waybar")
     hl.exec_cmd("awww-daemon")
